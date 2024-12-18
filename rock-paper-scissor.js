0eb234427fb9b7fae73163bfdcf70d96cb278b42
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2024-12-18 19:27:43 +0100

    version 1

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,9 +1,19 @@
-//  Functions
-
-// User Input
+// User Move
+const userMove = process.argv[2];
 
 // Computer Move
+const move = ["rock", "paper", "scissor"];
+const computerMove = move[Math.floor(Math.random() * 3)];
 
 // Compare
-
-// Result
+if (userMove === computerMove) {
+    console.log("It's a tie");
+} else if (userMove === "rock" && computerMove === "scissors") {
+    console.log("Player wins!");
+} else if (userMove === "paper" && computerMove === "rock") {
+    console.log("Player wins!");
+} else if (userMove === "scissors" && computerMove === "paper") {
+    console.log("Player wins!");
+} else {
+    console.log("Computer wins!");
+}
