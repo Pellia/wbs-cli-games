945332925955ddae6fe111a049ff0e7fe5b50070
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2024-12-24 21:54:06 +0100

    update caeser-cipher

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -7,20 +7,27 @@ function getUserShift() {
     return parseInt(process.argv[3]);
 }
 
+function encryptMsg(message, shift) {
+    let msg = "";
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    for (let i = 0; i < message.length; i++) {
+        if (message[i] === " ") {
+            msg = msg + " ";
+            continue;
+        }
+        const currentIndex = alphabet.indexOf(message[i]);
+        const newIndex = (currentIndex + shift) % 26;
+        msg = msg + alphabet[newIndex];
+    }
+    return msg;
+}
+
 // User Input
 const userInput = getUserInput();
 const userShift = getUserShift();
 
 // Encrypt
-for (let i = 0; i < userInput.length; i++) {
-    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    if (userInput[i] === " ") {
-        continue;
-    }
-    const currentIndex = alphabet.indexOf(userInput[i]);
-    const newIndex = (currentIndex + userShift) % 26;
-    userInput[i] = alphabet[newIndex];
-}
+const secretMsg = encryptMsg(userInput, userShift);
 
 // Log Result
-console.log(userInput);
+console.log(secretMsg);
